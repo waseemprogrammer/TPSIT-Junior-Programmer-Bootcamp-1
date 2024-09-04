a8d58642b0e3f8d70fc6695b7dea1f35ba0c1bfb
Waseem Programmer <waseemprogrammerofficial@gmail.com>
2024-09-04 18:14:51 +0500

    lecture 03 basics of Python

---
+++ lecture-03/README.md
@@ -0,0 +1,9 @@
+# Lecture 3 Python Basics Revision
+
+In this lecture we have revised our concept
+
+#### Assignment 
+
+You have to Pracitce about making python file, then practice variables and then perform basic arithmatic operations.
+
+#### Happy Coding.
---
+++ lecture-03/index.py
@@ -0,0 +1,12 @@
+
+#You guys have to make simple calculations based on user input,
+
+#For example, you can take user input and store it into variable then performe any arthmatic operations.
+
+inputFirstNumber = int(input("Please Enter Number to Calculate \n"))
+inputSecondNumber = int(input("Please Enter Second Number to Calculate \n"))
+# airthOperation = input("Enter What you Want to Do, Do you want to do - , + , * , /");
+
+ans = inputFirstNumber * inputSecondNumber
+
+print(f"Your Answer is: {ans}")
