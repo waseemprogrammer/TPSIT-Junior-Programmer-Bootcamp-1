b41b6be4306878c0e2bb1f6d672f738b5875bb57
Waseem Programmer <waseemprogrammerofficial@gmail.com>
2024-09-06 20:32:31 +0500

    Lecture 05 Data Types

---
+++ lecture-05/README.md
@@ -0,0 +1,9 @@
+# Lecture 05 
+
+in this Lecture we have Studied python data types in Detail
+
+
+### Assignment 
+
+Students you have to do practice by your Own whatever we have learned. 
+
---
+++ lecture-05/index.py
@@ -0,0 +1,62 @@
+# Python data Types. 
+
+# string 
+
+# username = "Abduallh"
+
+# # int 
+
+# stdRollNumber = 20
+
+# # float 
+
+# stdCGPA = 3.4
+
+# # complex 
+
+# stdID = 29309202399023434443434443j
+
+# # list
+
+# studentInfo = ["Ab", 2.4, 'Olevel']
+
+# # tuple
+
+# fruitsNames = ("Apple", "Banana", "Orange")
+
+# # Range
+
+# studentsTotalSubject = range(6)
+
+# # Dictionary
+
+# studentsInfo = {"Name":"Ab", "Class": "MSc", "ROllNumber": 2 }
+# # set
+
+# studentsInfo = {"Ab", "MSc", 2 }
+
+# # frozenset
+# studentsInfo = ({"Ab", "MSc", 2 })
+
+# # boolean
+
+# studentsAttendenceToday = True
+
+# print(type(stdCGPA))
+
+# convert number into float or complex data type
+
+
+# print("Enter your Favourite Number to Convert Data Type \n")
+# userInputNumber = int(input())
+
+# print("Grea Selection: We are Processing your data and Shortly You can See What datatype is this =>", userInputNumber)
+
+# # changedUserInputData = (userInputNumber)
+
+# print("Your Data type is: ", type(userInputNumber))
+
+PI = 3.12
+changeUseNameDataType = int(PI)
+print(type(changeUseNameDataType))
+
