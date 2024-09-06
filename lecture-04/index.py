37efccffb1c088981b41694b351bbcff6c2cd8b9
Waseem Programmer <waseemprogrammerofficial@gmail.com>
2024-09-06 19:49:51 +0500

    Lecture 04

---
+++ lecture-04/index.py
@@ -0,0 +1,39 @@
+# variables / containerr / data store koi box ha ya dabba ha 
+
+
+# arthimatic operations.  
+
+# sum + 
+# minus - 
+# modlus / Remainder %
+# division /
+# multiplication *
+
+# username isk andar kis trah ap rkhogy 
+
+# variable Naming Conventions 
+
+# camelCase 
+# myVariableName = "John"
+
+# snakecase 
+# my_variable_name = "John"
+
+# PascalCase
+# MyVariableName = "John"
+ 
+
+""" This is a comment
+written in
+more than just one line
+"""
+
+# data casting
+
+# username = str(3.3)
+# num1 = int(input("please Enterr number 1"));
+# print(num1 * 2)
+
+
+PI = float(3.14)
+print(type(PI))
