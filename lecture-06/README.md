ecbd7a1b82ea683e983e58e6d6461a26bcee4410
Waseem Programmer <waseemprogrammerofficial@gmail.com>
2024-09-09 20:03:05 +0500

    Lecture -06 strings

---
+++ lecture-06/README.md
@@ -0,0 +1,9 @@
+# lecture - 06
+
+in this lecture we have learned about remaings number topics in detail also we have covered about string and it's methods.
+
+
+### Assingment 
+
+You have to do all the Practice and Tomorrow you have to share me code which you have practiced. 
+
---
+++ lecture-06/index.py
@@ -0,0 +1,95 @@
+# import random
+
+# print(random.randrange(0, 100))
+
+# lecture -06
+
+# python numbers
+
+# int , float , complex 
+
+rollNumber = 20 
+
+PI = 3.14
+
+largeNumber = 3948838493834498j
+
+# print(rollNumber,PI,largeNumber)
+
+# what we do when to check datatype
+
+# print(type(largeNumber))
+
+# data type conversion
+
+floatRollNumber = float(rollNumber)
+
+complexPi = complex(PI)
+
+# print(type (floatRollNumber), floatRollNumber)
+# print(type (complexPi), complexPi)
+
+# floatLargeNumber = float(largeNumber)
+
+# print(floatLargeNumber, type(floatLargeNumber))
+
+justNumber = int(PI)
+
+# print(justNumber, type(justNumber))
+
+# Random Number 
+
+# import random 
+# random.randrange(0,10)
+
+# Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+# 'pakistan' or "Pakistan"
+
+# print('pakistan')
+
+# qoutes inside qouutes
+passengerName =  "jhon doe  from 'Germany'"
+
+print(passengerName)
+
+# multiline string 
+
+# paara = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua aaaakdjflaj sfdlajsdlfk 
+
+# akljsdflkjaskld fasdf"""
+
+# print(paara)
+
+
+# fruits = ['orange', 'banaaana', 'santra', 'apple']
+
+# print(fruits[1], fruits[2])
+
+# strings are arrays
+# fruit = 'oraange'
+
+# print(fruit[-1])
+
+# string length 
+
+# charLenth = 'hallo world'
+
+# print(len(charLenth))
+
+# string = 'how you doing?'
+
+# print('how' not in string)
+
+
+# excersise 
+
+# we need to check that user is known or not
+
+username = 'ghazi'
+
+print('ur' in username)
+# print("Welcome Mr: ", username)
