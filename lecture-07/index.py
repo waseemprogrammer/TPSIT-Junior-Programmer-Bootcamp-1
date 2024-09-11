bc04ca1a631a3c7be4906e8e2d93e5b5c4bc0bba
Waseem Programmer <waseemprogrammerofficial@gmail.com>
2024-09-11 20:27:14 +0500

    lecture 07

---
+++ lecture-07/index.py
@@ -0,0 +1,47 @@
+# python type casting
+username = str(' abdullah jutt ')
+# slicing strings
+
+print(username[0:5])
+
+# slicing from the start
+
+print(username[:5])
+
+# slicing from the end
+
+print(username[5:])
+
+# negative slicing
+
+print(username[-1:-3])
+
+# modify strings
+
+# uper string
+
+print(f"Here is your Uppercase String: {username.upper()}")
+
+# lower case string
+
+print(f"Here is your Lower Case String: {username.lower()}")
+
+
+print(username)
+
+# total characters
+
+print(len(username))
+
+# remove whitespace
+
+print(username)
+print(username.strip())
+
+#   REPLACE specific character from string
+
+print(username.replace('abdullah', 'yousaf'))
+
+print(type(username))
+# split into array / list 
+# print(((len(username.strip().split()))))
