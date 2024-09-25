6279587cb1e65c7259e65b1f78717415b3bc0e42
Waseem Programmer <waseemprogrammerofficial@gmail.com>
2024-09-25 19:54:08 +0500

    lecture -09

---
+++ lecture-09/index.py
@@ -0,0 +1,23 @@
+# username = str(input("Please enter your Username"))
+# string concatination
+
+# print("Welcome MR/Ms. => " + username)
+
+# stirng concatination using f string method
+
+# print(f"Welcome MR. {username}")
+
+# number1 =  20
+
+# number2 = 30
+    
+# print(f"Your Number Total  is : \n {20 + 30}")
+
+txt = "We are the \t so-called \"Vikings\" from the north."
+print(txt)
+
+# python booleans
+
+# true / false
+price = 0
+print(bool(price))
