468238c236b3d303ccd85af874246a6e33ed0878
Waseem Programmer <waseemprogrammerofficial@gmail.com>
2024-09-25 20:20:54 +0500

    lecture 10 Python Arithmatic and Asisngment Operators

---
+++ lecture-10/README.md
@@ -0,0 +1,6 @@
+# lecutre 10
+
+## Python Operators
+
+### in this chapter we have studied about python operatoors, artihmatic operatiors and assignement operators.
+
---
+++ lecture-10/index.py
@@ -0,0 +1,38 @@
+# operators in python
+# Python Arithmetic Operators
+
+# take inpuut from user
+
+# num1  = int(input("Enter Number1"))
+# num2  = int(input("Enter Number2"))
+
+# sum = num1 + num2
+# minus = num1 - num2
+# multply = num1 * num2
+# division = num1 / num2
+# floor = num1 // num2
+# moduls = num1 % num2
+# exponent = num1 ** num2
+
+# print(f"sum => {sum}, minus => {minus}, multiply => {multply} , division => {division}, floor => {floor}, moduls => {moduls}, exponent=> {exponent}")
+
+
+# Python Assignment Operators
+
+# yousaf = 50
+price = 50
+# yousaf = yousaf + 50
+# price = price + 50
+# price += 50
+# price -= 50
+# price *= 50
+# price = price / 50
+# price /= 50
+# price %= 50
+# price //= 50
+
+# price **= 50
+# price = price & 50
+# price &= 50
+price |= 50
+print(price)
