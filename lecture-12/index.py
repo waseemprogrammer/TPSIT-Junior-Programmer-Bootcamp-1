65a774d829204e306a24954503b85ef838c0ccdc
futurewebprogramming <123332352+futurewebprogramming@users.noreply.github.com>
2024-09-30 20:34:52 +0500

    Lecture 12 python Simple Calculator

---
+++ lecture-12/index.py
@@ -0,0 +1,27 @@
+# arithmatic opereations
+
+
+
+# +
+# -
+# /
+# *
+# %
+
+# problem => make a simple calculator who takes input from user and then it simply calculate and return it sum of that given values from user.
+
+userInputNum1 = int(input("Enter your Number 1 which you want to calculate \n"))
+userInputNum2 = int(input("Enter your Number 2 Which you want to calculate \n"))
+
+sum = userInputNum1 + userInputNum2
+
+minus = userInputNum1 - userInputNum2
+multiply = userInputNum1 * userInputNum2
+division = userInputNum1 / userInputNum2
+reminder = userInputNum1 % userInputNum2
+
+print("Herre is the sum of Two Numbers:", sum)
+print("Herre is the Subtraction of Two Numbers ", minus)
+print("Herre is the multiply of Two Numbers ", multiply)
+print("Herre is the division of Two Numbers ", division)
+print("Herre is the remainder of Two Numbers ", reminder)
