64812c4d88a38ec91815e6b06256e6c9a601fd87
futurewebprogramming <123332352+futurewebprogramming@users.noreply.github.com>
2024-10-01 20:26:51 +0500

    lecture 13 operators

---
+++ lecture-13/README.md
@@ -0,0 +1,10 @@
+# Lecture 13
+
+in this chapter we have studied about Python Comparison Operators , python logical operators , python identity operator & Operator Precedence
+
+
+#### Assignment 
+
+you have to practice all of these operators.
+
+
---
+++ lecture-13/index.py
@@ -0,0 +1,57 @@
+# Python Comparison Operators
+
+
+# price = 21
+
+# print(price == 20)
+# print(price != 20)
+# print(price > 20)
+# print(price < 20)
+# print(price >= 20)
+# print(price <= 20)
+
+# python logical operators
+
+# productPrice = 5
+
+# print (productPrice > 5 and productPrice < 6)
+# print (productPrice <= 5 or productPrice < 6)
+# print (not(productPrice == 6 and productPrice == 6))
+
+# python identity operator
+
+# is 
+# x = ['orange', 'mango']
+# y = ['orange', 'mango']
+
+# z = x 
+
+# print( x is z) 
+
+
+# print(x is y)
+
+# print(x == y)
+
+# is not
+
+# print(x is not y)
+
+# z = x
+# print(x is not z)
+
+# Python Bitwise Operators
+
+
+# Operator Precedence
+
+
+# BODMAS rule 
+
+# B - Brackets
+# O - Order of powers or roots
+# D - Division
+# M - Multiplication
+# A - Addition
+
+print((6 + 3) -  6 + 3)
