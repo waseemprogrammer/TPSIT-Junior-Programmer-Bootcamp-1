57ad6dd3a6ef84e817fdb742ed1649161f2df286
futurewebprogramming <123332352+futurewebprogramming@users.noreply.github.com>
2024-10-03 20:14:12 +0500

    Lecture 14- Python Lists

---
+++ lecture-14/README.md
@@ -0,0 +1,8 @@
+# Lecture - 14 Python Lists 
+
+we have studied python list in detail, 
+how to access item, add item, replace item
+range, negative index range etc.
+
+### Assingemnt
+You have to practice all of this and show tomorrow.
---
+++ lecture-14/index.py
@@ -0,0 +1,37 @@
+fruits = ["apple", "banana", "cherry", "strawberry", "Apple", 2239, 2.2, True]
+# fruits[0] = "cherrry" replace value
+# add value
+# fruits.append('Orange')
+# allow to add duplicate values
+# fruits.append("cherry")
+# check how many items are in the list
+# print(len(fruits))
+
+# check data type
+# type(fruits)
+# print(type(fruits))
+
+# define list using list constructor
+
+# weather = list(("Autm", 'Winter', "Summer"))
+# print(weather)
+
+# range of indexes
+
+# print(fruits[2:5])
+# print first tow values
+# print(fruits[:2])
+
+# print after two values
+
+# print(fruits[2:])
+
+
+# Range of Negative Indexes
+
+# print(fruits[-4:-1])
+
+# Check if Item Exists
+
+if "banana" in fruits:
+    print("Yes")
