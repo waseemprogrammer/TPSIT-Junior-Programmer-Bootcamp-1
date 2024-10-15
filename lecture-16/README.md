752b42cfe789b3b062722e2c4434946e8f615cde
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2024-10-15 20:19:07 +0500

    lecture 16 List method looping through collection /list

---
+++ lecture-16/README.md
@@ -0,0 +1,5 @@
+# Lecture 16 - Lists Methods
+
+### Assignment 
+
+You have to practice previous lecture and also tryy to print collection items one by one using for loop
---
+++ lecture-16/index.py
@@ -0,0 +1,6 @@
+# Loop Through a List
+# You can loop through the list items by using a for loop:
+numbers = [2,943034, 3249043, 3849]
+
+for okay in numbers:
+    print(okay)
