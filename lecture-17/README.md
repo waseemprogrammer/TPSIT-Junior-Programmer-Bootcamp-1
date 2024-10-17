68cb6152bb198ee3ae5c93ec52622e7306a97c77
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2024-10-17 20:09:53 +0500

    lecture 17 loop through list and list comprehension

---
+++ lecture-17/README.md
@@ -0,0 +1,5 @@
+# Lecture -17 
+
+Loop Through a List
+we have studied looping through list and also we have studied how we can loop through via index numbers and at the end we have learned about list comprehension, shorter syntax of looping through list.
+
---
+++ lecture-17/index.py
@@ -0,0 +1,29 @@
+# fruits = ['apple', 'banana', 'cherry']
+
+# i want one by one these variables should be print on screen
+
+# for fruit in fruits:
+#     print(fruit)
+    
+# Loop Through the Index Numbers
+  
+# fruits = ["apple", "banana", "cherry"]
+
+# for index in range(len(fruits)):
+#     print(fruits[index])
+    
+
+# for i in range(len(thislist)):
+#   print(thislist[i])
+
+# Looping Using List Comprehension
+
+
+# fruits = ["apple", "banana", "cherry"]
+
+# # for x in fruits:
+# #     print(x)
+# # [print(fruit) for fruit in fruits]
+# [print(fruit) for fruit in fruits]
+
+
