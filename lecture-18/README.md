cb6f9cd68d4012f6607c0bd4b2ca25296a4a7b80
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2024-10-25 19:56:24 +0500

    Lecture 18 List sorting

---
+++ lecture-18/README.md
@@ -0,0 +1,7 @@
+# Lecture - 18 List Sorting 
+
+we have studied how to sort list, assending order, descending  order, case insentive , reverse order. 
+
+# Assignemnt 
+
+You guys have to pracitce remaingings topic and this topic.
---
+++ lecture-18/index.py
@@ -0,0 +1,43 @@
+# Python - Sort Lists 
+
+# (ascending order)
+
+# fruits = ["orange", "mango", "kiwi", "pineapple", "banana", "Apple"]
+
+# fruits.sort()
+
+# print(fruits)
+
+
+# Descening order sorting
+
+# fruits = ["orange", "mango", "kiwi", "pineapple", "banana", "Apple"]
+
+# fruits.sort(reverse=True)
+
+# print(fruits)
+
+
+# customizing 
+
+# def myfunc(n):
+#   return abs(n  - 50)
+
+# numberlist = [100, 50, 65, 82, 23]
+# numberlist.sort(key=myfunc)
+# print(numberlist)
+
+# case-insensitive 
+# fruits = ["banana", "Orange", "Kiwi", "cherry"]
+
+# fruits.sort(key=str.lower)
+# print(fruits)
+
+# Reverse Order
+
+# fruits = ["kivi", "cherry", "banana"]
+
+
+# fruits.reverse()
+# print(fruits)
+
