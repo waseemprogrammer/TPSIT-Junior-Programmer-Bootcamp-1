a7075ea5b399bf3d262644ccb84005b12126b1a1
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2024-10-28 20:50:34 +0500

    lecture 19 List methods Join , Copy and Excercies

---
+++ lecture-19/README.md
@@ -0,0 +1,7 @@
+# lectue # 19 List methods, Copy , Join and Few Exercise
+
+in this lecture we have studied in detail how we can make copy of listt, how we can join lists and excerciese.
+
+# Assingment 
+
+You have to practice all.
---
+++ lecture-19/index.py
@@ -0,0 +1,61 @@
+# how to copy list in pythhon
+
+# using copy() method
+# fruits = ["Mango", "orange", "chery"]
+
+# fruitsCopy = fruits.copy()
+
+# print(fruitsCopy[1])
+
+# using list method
+
+# fruits = ["oraange", 'Santrra', 'kela']
+
+# newFruits = list(fruits)
+
+# print(newFruits[-1])
+
+# Use the slice Operator
+
+# car = ['maruti', 2023,'white']
+
+# mehran = car[:]
+
+# print(mehran[1])
+
+# Python - Join Lists
+
+
+# using + operator 
+
+# summerFruits = ["kela", 'apple', 'saib']
+
+# # winterFruits = ['malta','anar', 'japantomato']
+
+# # fruits = summerFruits + winterFruits
+
+# # print(fruits)
+
+# using append method 
+
+# summerFruits = ["kela", 'apple', 'saib']
+
+# winterFruits = ['malta','anar', 'japantomato']
+
+# for fruits in summerFruits:
+#     winterFruits.append(fruits)
+
+# print(winterFruits)
+
+# extend() method
+
+
+# summerFruits = ["kela", 'apple', 'saib']
+
+# winterFruits = ['malta','anar', 'japantomato']
+
+
+# summerFruits.extend(winterFruits)
+
+# print(summerFruits)
+
